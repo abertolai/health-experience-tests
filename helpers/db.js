d54edf7708cff8ed9d66d7fff906405b5baa7eb1
Amanda Bertolai <amandarbertolai@gmail.com>
2023-08-05 15:50:36 -0300

    Implementando a API Helpers para ajudar na criação de massa para os testes

---
+++ helpers/db.js
@@ -0,0 +1,93 @@
+require("dotenv").config();
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+const deleteAndCreateStudent = (req, res) => {
+  const student = req.body;
+  const query = `
+                    WITH add AS (
+                        INSERT INTO students (name, email, age, weight, feet_tall) 
+                        VALUES ($1, $2, $3, $4, $5)
+                    )
+                    DELETE FROM students WHERE email = $2;
+                `;
+
+  const values = [
+    student.name,
+    student.email,
+    student.age,
+    student.weight,
+    student.feet_tall,
+  ];
+
+  pool.query(query, values, function (error, result) {
+    if (error) {
+      return res.status(500).json(error);
+    }
+    res.status(201).json(result);
+  });
+};
+
+const insertEnrollByEmail = (req, res) => {
+  const { email, plan_id, price } = req.body;
+  const query = `
+                    INSERT INTO enrollments (enrollment_code, student_id, plan_id, credit_card, status, price)
+                        SELECT
+                        'XPTO123' as enrollment_code,
+                        id as student_id,
+                        $2 as plan_id,
+                        '4242' as credit_card,
+                        true as status,
+                        $3 as price
+                        FROM students
+                        WHERE email = $1;     
+                `;
+
+  const values = [email, plan_id, price];
+
+  pool.query(query, values, function (error, result) {
+    if (error) {
+      return res.status(500).json(error);
+    }
+    res.status(201).end();
+  });
+};
+
+const deleteStudentByEmail = (req, res) => {
+  const studentEmail = req.params.email;
+  const query = "DELETE FROM students WHERE email = $1;";
+
+  pool.query(query, [studentEmail], function (error) {
+    if (error) {
+      return res.status(500).json(error);
+    }
+    res.status(204).end();
+  });
+};
+
+const selectStudent = (req, res) => {
+  const studentEmail = req.params.email;
+  const query = "SELECT id FROM students WHERE email = $1;";
+
+  pool.query(query, [studentEmail], function (error, result) {
+    if (error) {
+      return res.status(500).json(error);
+    }
+    res.status(200).json(result.rows[0]);
+  });
+};
+
+module.exports = {
+  deleteAndCreateStudent,
+  deleteStudentByEmail,
+  selectStudent,
+  insertEnrollByEmail,
+};
